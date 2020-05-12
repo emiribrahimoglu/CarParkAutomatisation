e0b9252293c3061ac0ab608770bf81441847e513
Hasan <zhasanozer@hotmail.com>
2020-05-12 18:20:22 +0300

    deneme3

--- CarParkAutomatisation/Form1.Designer.cs
+++ CarParkAutomatisation/Form1.Designer.cs
@@ -31,6 +31,7 @@
         {
             this.metroButton1 = new MetroFramework.Controls.MetroButton();
             this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.metroListView1 = new MetroFramework.Controls.MetroListView();
             this.SuspendLayout();
             // 
             // metroButton1
@@ -51,11 +52,24 @@
             this.metroButton2.Text = "metroButton2";
             this.metroButton2.UseSelectable = true;
             // 
+            // metroListView1
+            // 
+            this.metroListView1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.metroListView1.FullRowSelect = true;
+            this.metroListView1.Location = new System.Drawing.Point(283, 297);
+            this.metroListView1.Name = "metroListView1";
+            this.metroListView1.OwnerDraw = true;
+            this.metroListView1.Size = new System.Drawing.Size(121, 97);
+            this.metroListView1.TabIndex = 2;
+            this.metroListView1.UseCompatibleStateImageBehavior = false;
+            this.metroListView1.UseSelectable = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1066, 554);
+            this.Controls.Add(this.metroListView1);
             this.Controls.Add(this.metroButton2);
             this.Controls.Add(this.metroButton1);
             this.Margin = new System.Windows.Forms.Padding(2);
@@ -69,5 +83,6 @@
 
         private MetroFramework.Controls.MetroButton metroButton1;
         private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroListView metroListView1;
     }
 }
